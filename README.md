1a233980bd22ff57fdd61803b5177198dfd4a65c
Juan <juanmireles@proton.me>
2025-07-19 20:16:42 -0400

    setup cmake

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # space-shooter-game
+
+## Requirements
+
+- C++23
+- SDL3-devel
+- cmake
+
---
+++ build/CMakeFiles/space-shooter.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/space-shooter.dir/main.cpp.o"
+  "CMakeFiles/space-shooter.dir/main.cpp.o.d"
+  "space-shooter"
+  "space-shooter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/space-shooter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
